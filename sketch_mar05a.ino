3fbc7c552adab5699f1716992f5eced8492ae47c
Divye Khanna <divye@dnkinfra.com>
2016-04-14 23:55:30 +0530

    Prototype

---
+++ sketch_mar05a.ino
@@ -0,0 +1,19 @@
+int led = 12;
+int counter = 0;
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(led, OUTPUT);
+  Serial.begin(115200);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  digitalWrite(led, HIGH);
+  counter += 1;
+  Serial.println(Serial.readString());
+  Serial.print(counter);
+  delay(1000);
+  digitalWrite(led, LOW);
+  delay(1000);
+}
